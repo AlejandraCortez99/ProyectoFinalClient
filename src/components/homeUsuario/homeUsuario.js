dd1b1a063ed0db9dc672b446d516707b7bdebdaa
AlejandraCortez99 <alejandracl.1999@hotmail.com>
2021-07-10 18:24:29 +0200

    Ahora sí entendí

--- src/components/homeUsuario/homeUsuario.js
+++ src/components/homeUsuario/homeUsuario.js
@@ -8,6 +8,7 @@ const HomeUsuario = () => {
     nombre: "",
     favoritos: [],
   });
+  
   const getUsuario = async () => {
     let responseFromGet = await fetch("http://localhost:2550/homeUsuario", {
       method: "GET",
@@ -22,13 +23,8 @@ const HomeUsuario = () => {
         return result;
       });
       console.log(responseFromGet);
+      setInfo({ nombre: responseFromGet.nombre, favoritos: responseFromGet.favoritos})
     };
-      return (
-          <div className="home-usuario-container">
-              <h1>Ruta Privada</h1>
-          </div>
-      );
-    //   console.log(responseFromGet);
     //       let nombreUsuario = responseFromGet.nombre;
     //       let favoritosUsuario = responseFromGet.favoritos;
     //       let guardados = [];
@@ -55,32 +51,40 @@ const HomeUsuario = () => {
     //         });
     //       };
 
-    //       useEffect(() => {
-    //         getUsuario();
-    //       }, []);
+          useEffect(() => {
+            getUsuario();
+          }, []);
 
-    // if (info.nombre === "") {
-    //     return <div>Cargando...</div>;
-    //   } else {
-    //     return (
-    //         <div>
-    //         <p>Welcome {info.nombre}</p>
-    //         <table>
-    //           <caption>Your recipes</caption>
-    //           <tbody>
-    //             {info.favoritos.map((favorito) => {
-    //               return (
-    //                 <tr key={`favorito-container-${favorito.id}`}>
-    //                   <td key={`favorito-titulo-${favorito.id}`}>{favorito.titulo}</td>
-    //                   <td key={`favorito-artista-${favorito.id}`}>{favorito.artista}</td>
-    //                   <td key={`favorito-album-${favorito.id}`}>{favorito.album}</td>
-    //                 </tr>
-    //               );
-    //             })}
-    //           </tbody>
-    //         </table>
-    //       </div>
-    //     );
+    if (info.nombre === "") {
+        return <div>Cargando...</div>;
+      } else {
+        return (
+            <div className="home-usuario-container">
+            <p>Welcome {info.nombre}</p>
+            <table>
+              <caption>
+                Tus favoritos
+              </caption>
+              <tbody>
+                {info.favoritos.map((favorito) => {
+                  return (
+                    <tr key={`favorito-container-${favorito.id}`}>
+                      <td key={`favorito-titulo-${favorito.id}`}>{favorito.titulo}</td>
+                      <td key={`favorito-autor-${favorito.id}`}>{favorito.autor}</td>
+                      <td key={`favorito-album-${favorito.id}`}>{favorito.album}</td>
+                    </tr>
+                 );
+                 })
+                 }
+              </tbody>
+            </table>
+          </div>
+        )};
+    // return (
+    //     <div className="home-usuario-container">
+    //         <h1>Ruta Privada</h1>
+    //     </div>
+    // );
 
 };
 export default HomeUsuario;
